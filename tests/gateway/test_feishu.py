9e13cfbc37c93f55a0a5e3aa8f048d8e7411c46b
lazydao <daodao1990@gmail.com>
2026-08-26 17:58:17 +0800

    test(feishu): align existing-topic reply semantics

--- tests/gateway/test_feishu.py
+++ tests/gateway/test_feishu.py
@@ -1192,7 +1192,7 @@ class TestAdapterBehavior(unittest.TestCase):
 
 
     @patch.dict(os.environ, {}, clear=True)
-    def test_send_document_reply_uses_thread_flag(self):
+    def test_send_document_reply_omits_thread_flag_for_existing_topic(self):
         from gateway.config import PlatformConfig
         from plugins.platforms.feishu.adapter import FeishuAdapter
 
@@ -1244,7 +1244,7 @@ class TestAdapterBehavior(unittest.TestCase):
             os.unlink(file_path)
 
         self.assertTrue(result.success)
-        self.assertTrue(captured["request"].request_body.reply_in_thread)
+        self.assertIsNone(captured["request"].request_body.reply_in_thread)
 
 
     @patch.dict(os.environ, {}, clear=True)
@@ -2465,5 +2465,4 @@ class TestChatLockEviction(unittest.TestCase):
 
         adapter = self._make_adapter()
         self.assertIsInstance(adapter._chat_locks, _collections.OrderedDict)
-
 
